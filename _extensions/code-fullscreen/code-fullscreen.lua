ee778d696bb939f6b251644270a1373451a2e594
shafayetShafee <seaborn.sicario@gmail.com>
2023-02-11 11:43:49 +0600

    revealjs working good

--- _extensions/code-fullscreen/code-fullscreen.lua
+++ _extensions/code-fullscreen/code-fullscreen.lua
@@ -17,27 +17,6 @@ local function ensure_html_deps()
 end
 
 
-local function ensure_revealjs_deps()
-  quarto.doc.add_html_dependency({
-    name = "jquery",
-    version = "3.6.3",
-    scripts = {
-      { path = "asset/jquery-3.6.3.min.js"}
-    }
-  })
-  quarto.doc.add_html_dependency({
-    name = "code-fullscreen",
-    version = "1.0.0",
-    scripts = {
-      { path = "resources/js/reveal-code-fullscreen.js", attribs = {defer = "true"} }
-    },
-    stylesheets = {"resources/css/reveal-code-fullscreen.css"}
-  })
-end
-
-
-if quarto.doc.is_format('html') then
+if quarto.doc.is_format('html:js') then
   ensure_html_deps()
-elseif quarto.doc.is_format('revealjs') then
-  ensure_revealjs_deps()
 end
